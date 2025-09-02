c753781691568830a3a4be2c42317f7459a08520
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-02 11:20:28 +0530

    refactor: update Providers component to use queryClient and add Toaster

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { queryClient } from "./lib/queryClient";
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-
+            <Toaster />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
